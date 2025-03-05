6400f797daf52133acfd6ab2ce5566acaa08bcf9
Costi Ciudatu <ciudatu@adobe.com>
2025-03-05 17:07:47 +0200

    Custom adobe version and artifactory publish

--- build-linux-x86_64.sh
+++ build-linux-x86_64.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-export DOCKER_BUILDKIT=1
-
-root_dir="$(dirname $0)"
-
-docker build \
-  --ssh default \
-  --progress=plain \
-  --platform linux/x86_64 \
-  -f Dockerfile-build-wheel.x86_64 \
-  -t docker-cja-arrow-dev.dr-uw2.adobeitc.com/datafusion-python:44.0.0-amd64 \
-  "$root_dir"
-
-id=$(docker create --platform linux/amd64 docker-cja-arrow-dev.dr-uw2.adobeitc.com/datafusion-python:44.0.0-amd64)
-docker cp $id:/datafusion-44.0.0-cp38-abi3-manylinux_2_28_x86_64.whl .
-docker rm -v $id
+build-linux.sh
